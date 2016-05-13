e72675e3a56117841f89be00c30c462d81b47e06
kotofei4 <kotofei4@gmail.com>
2016-05-13 14:42:50 +0300

    fuck yeah

--- src/AppBundle/Controller/ClassTester.php
+++ src/AppBundle/Controller/ClassTester.php
@@ -19,11 +19,13 @@ class ClassTester extends Controller
         $xxx->setFcVar("10");
         $var1 = $xxx->sayHi();
         $var2 = $xxx->getFcVar();
+        $var3 = 'fuck yeah';
 
         $templating = $this->container->get('templating');
         $html = $templating->render('users/users.html.twig', [
             'var1' => $var1,
-            'var2' => $var2
+            'var2' => $var2,
+            'var3' => $var3
         ]);
 
         return new Response($html);
